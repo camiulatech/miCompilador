e4758015b867c2905e11ebd36e6b787c466e35e0
Camila <caulate@estudiantec.cr>
2024-10-08 11:20:43 -0600

    Cambio en el formato de errores

--- src/FaseSintactica.java
+++ src/FaseSintactica.java
@@ -5,17 +5,18 @@ import java.util.List;
 public class FaseSintactica {
     private List<Token> tokens;
     private int indiceActual;
+    private int lineaActual;
     private boolean validar_parentesis;
     private boolean validar_numeroSolo;
     private List<Character> lista = new ArrayList<>();
     private List<Character> lista_numero = new ArrayList<>();
 
-
     public FaseSintactica(List<Token> tokens) {
         this.tokens = tokens;
         this.indiceActual = 0;
-        lista.add('N'); // N es igual a No tiene par el parentesis inicial
-        lista_numero.add('N');
+        this.lineaActual = 0;
+        lista.add('N'); // N es igual a NO tiene par el parentesis inicial
+        lista_numero.add('N'); // N es igual a NO es una asignacion
         this.validar_parentesis = false;
         this.validar_numeroSolo = false;
     }
@@ -26,20 +27,21 @@ public class FaseSintactica {
                 programa(); // Comienza con la producción 'programa'
             }
         } catch (Exception e) {
-            System.out.println("Error de análisis sintáctico: " + e.getMessage());
+            System.out.println("Error [Fase Sintactica]: La linea " + (lineaActual) + " " + e.getMessage());
         }
     }
 
     private void programa() throws Exception {
         // La estructura es 'expresion ; { programa }'
         while (indiceActual < tokens.size()) {
+            lineaActual++;
             expresion(); // Analiza una expresión
 
             if (tokens.get(indiceActual).getTipo().equals("PUNTO_COMA")) {
                 lista_numero.add('N');
                 siguienteToken(); // Consume el punto y coma
             } else {
-                throw new Exception("Se esperaba un punto y coma al final de la expresión.");
+                throw new Exception("Error [Fase Sintactica]: La linea " + (lineaActual) + " contiene un error en su gramática, falta token ;");
             }
         }
     }
@@ -54,7 +56,7 @@ public class FaseSintactica {
             if (tokens.get(indiceActual).getTipo().equals("ASIGNACION")) {
                 siguienteToken(); // Consume el signo '='
                 // Analiza la expresión del lado derecho
-                lista_numero.add('S');
+                lista_numero.add('S'); // Se verifica que el numero es parte de una asignacion
                 expresion(); 
 
             } else {
@@ -66,9 +68,7 @@ public class FaseSintactica {
                         tokens.get(indiceActual).getTipo().equals("RESTA"))) {
                     siguienteToken(); // Consume el operador
                     termino(); // Consume el siguiente término
-
                 }
-
             }
         } else {
             // Si no es un identificador, se comienza con un término
@@ -88,15 +88,13 @@ public class FaseSintactica {
         //System.out.println(lista);
         System.out.println(tokens.get(indiceActual).getTipo());
 
-
         if ((tokens.get(indiceActual).getTipo().equals("PARENTESIS_DER")) && (lista.get(lista.size()-1) == 'N')) {
-            //System.out.println("Falta izq");
             validar_parentesis = true;
         } 
 
         if (!validar_parentesis) {
         } else {
-            throw new Exception("Falta izq");
+            throw new Exception("contiene un error en su gramatica, falta token ( (parentesis izquierdo)");
         }
 
         
@@ -125,13 +123,12 @@ public class FaseSintactica {
             //System.out.println(lista_numero);
             //System.out.println("VALIDAR NUMERO ="+ tokens.get(indiceActual));
             if(tokens.get(indiceActual).getTipo().equals("PUNTO_COMA") && (lista_numero.get(lista_numero.size()-1) == 'N')){
-                System.out.println("ES UN NUMERO SOLO ");
                 validar_numeroSolo = true;
             }
              
             if (!validar_numeroSolo) {
             } else {
-                throw new Exception("Numero solito");
+                throw new Exception("contiene un error en su gramática, el numero esta solo");
             }
             
         } else if (tokens.get(indiceActual).getTipo().equals("PARENTESIS_IZQ")) {
@@ -142,11 +139,11 @@ public class FaseSintactica {
             if (tokens.get(indiceActual).getTipo().equals("PARENTESIS_DER")) {
                 siguienteToken(); // Consume ')'
             } else {
-                throw new Exception("Se esperaba un paréntesis derecho.");
+                throw new Exception("Se esperaba un parentesis derecho.");
             } 
             lista.add('N');
         } else {
-            throw new Exception("Se esperaba un identificador, número o un paréntesis.");
+            throw new Exception("Se esperaba un identificador, numero o un parentesis.");
         }
     }
 
